3bba25d90858ad5c99bfdc33dc53fbcbbf30b9eb
rafarem <edgarembao@hotmail.com>
2021-11-08 09:40:44 -0700

    subir cambios

--- paneles/paneles/context_processor.py
+++ paneles/paneles/context_processor.py
@@ -3,9 +3,9 @@ from apps.home.models import Configuracion
 from django.conf import settings
 
 def add_variable_context(request):
-    configuracion = Configuracion.objects.get(pk=1)
-    print(configuracion.carga)
+   
+    
     return{
-        'configuracion': configuracion,
+        '
         'settings': settings
     }
